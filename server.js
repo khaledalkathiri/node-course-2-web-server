12ca58a8af43b628abf0685bcaf546b2486de6b9
khaled Alkathiri <kk-kk20@hotmail.com>
2017-12-28 00:52:18 +0300

    Setting up Script and Heroku port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
-
+var port = process.env.PORT || 3000; //set the port through Heroku or assign it to the default
 var app = express();
 
 app.set("view engine", "hbs");
@@ -46,4 +46,6 @@ app.get('/about', (req,res) =>{
     });
 });
 
-app.listen(3000);
+app.listen(port, ()=>{
+    console.log(`Server is up  on port ${port}`);
+});
