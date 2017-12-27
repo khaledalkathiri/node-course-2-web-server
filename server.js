a693d2d990a6272a75dd096f34c747194d1766ff
khaled Alkathiri <kk-kk20@hotmail.com>
2017-12-27 23:57:49 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+
+var app = express();
+
+app.set("view engine", "hbs");
+
+//middleware very usefull with logs
+app.use( (req, res, next) =>{
+    var now = new Date().toString();
+    var log =  `${now}:   ${req.method}  : ${req.url}`;
+    console.log(log);
+    fs.appendFile("server.log", log + '\n', (err) => {
+        if(err){
+            console.log("Unable to append the file");
+        }
+    });
+    next();
+});
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+hbs.registerHelper('getCurrentYear', () =>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) =>{
+    res.render('home.hbs', {
+        welcomeMessage: "Welcome to my website",
+        pageTitle: "Home page"     
+    });
+});
+
+
+app.get('/about', (req,res) =>{
+    res.render('about.hbs', {
+        pageTitle: "About page"
+    });
+});
+
+app.listen(3000);
